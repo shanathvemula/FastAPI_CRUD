39d81f003b6ad22ca311090c40dd8bc157f1fa73
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2023-09-26 13:24:32 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 from bson import ObjectId
 
 DBClient = MongoClient("mongodb://localhost:27017/")
+
+DBName = 'blogs'
+Collection = 'blog'
