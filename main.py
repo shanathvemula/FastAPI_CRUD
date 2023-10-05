7329f04680b0d1d117d49aa212ad301025866e6f
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2023-10-05 12:15:22 +0530

    Update main.py with JWT Authentication

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import dateutil.utils
 import pymongo
 
 import uvicorn
-from fastapi import FastAPI, APIRouter, status
+from fastapi import FastAPI, APIRouter, status, HTTPException, Request, Response, Depends
+from fastapi.responses import JSONResponse
 from datetime import datetime
 from typing import Optional
 from uuid import UUID, uuid4
 # from pymongo import MongoClient
-from Schemas import User, Blog, UserCreate
+from Schemas import User, Blog, UserCreate, SecretStr
 from database import DBClient, ObjectId, DBName, Collection, ASCENDING
+from authentication import AccessToken, RefreshToken, VerifyToken, jwtBearer
+
+# from fastapi.security
 
 # Initializing the App
 app = FastAPI()
@@ -18,6 +22,43 @@ app = FastAPI()
 # Connecting to db
 # DBClient = MongoClient('mongodb://localhost:27017/')
 
+# print(list(DBClient[DBName]['user'].find()))
+@app.post('/access-token')
+def token(cred: dict):
+    user = list(
+        DBClient[DBName]['user'].find({'$and': [{'username': cred['username']}, {'password': cred['password']}]}))
+    if len(user) == 1:
+        return AccessToken(user[0])
+    else:
+        return {"Error": "With given credential user doesn't exists"}
+
+
+@app.post('/refresh-token')
+def Refresh_Token(request: dict):
+    try:
+        if request['token']:
+            vt = RefreshToken(request)
+            return JSONResponse(status_code=200, content=vt)
+        else:
+            raise ValueError("Token required")
+    except Exception as e:
+        if str(e) == 'Signature has expired':
+            return JSONResponse(content={"Error": str(e)}, status_code=401)
+        return JSONResponse(content={"Error": str(e)}, status_code=400)
+
+
+@app.post('/verify-token')
+def Verify_Token(request: dict):
+    try:
+        if request['token']:
+            vt = VerifyToken(request)
+            return JSONResponse(status_code=200, content=vt)
+        else:
+            raise ValueError("Token required")
+    except Exception as e:
+        if str(e) == 'Signature has expired':
+            return JSONResponse(content={"Error": str(e)}, status_code=401)
+        return JSONResponse(content={"Error": str(e)}, status_code=400)
 
 # Default Route
 @app.get("/")  # decorator for the get request
@@ -37,15 +78,16 @@ def read_item(item_id: int, q: str):
 
 # CRUD Operations
 # Create Operation
-@app.post("/blog/", status_code=status.HTTP_201_CREATED)
-def create_blog(request: Blog):
+@app.post("/blog/", status_code=status.HTTP_201_CREATED, dependencies=[Depends(jwtBearer())])
+def create_blog(blog: Blog, request: Request):
     try:
-        bl = dict(request)
-        res = DBClient[DBName][Collection].insert_one(dict(bl))
+        bl = dict(blog)
+        print(bl)
+        res = DBClient[DBName][Collection].insert_one(dict(blog))
         bl['_id'] = str(res.inserted_id)
         return bl
     except Exception as e:
-        return {"Error": "Document is not created"}
+        return JSONResponse({"Error": "Document is not created"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Getting By Oid
@@ -58,24 +100,24 @@ def get_blog(Oid):
         res['_id'] = str(res['_id'])
         return res
     except Exception as e:
-        return {"Error": str(e)}
+        return JSONResponse({"Error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Update By Oid
-@app.put('/blog/{Oid}')
-@app.patch('/blog/{Oid}')
-def blog_update(Oid, request: Blog):
+@app.put('/blog/{Oid}', dependencies=[Depends(jwtBearer())])
+@app.patch('/blog/{Oid}', dependencies=[Depends(jwtBearer())])
+def blog_update(Oid, blog: Blog):
     try:
-        res = DBClient[DBName][Collection].update_one({'_id': ObjectId(Oid)}, {'$set': dict(request)})
+        res = DBClient[DBName][Collection].update_one({'_id': ObjectId(Oid)}, {'$set': dict(blog)})
         if res.raw_result['updatedExisting']:
             return {"Message": "Data Updated Successfully"}
         return {"Error": "Data Not Updated"}
     except Exception as e:
-        return {"Error": str(e)}
+        return JSONResponse({"Error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Delete By Oid
-@app.delete('/blog/{Oid}')
+@app.delete('/blog/{Oid}', dependencies=[Depends(jwtBearer())])
 def blog_delete(Oid):
     res = DBClient[DBName][Collection].delete_one({'_id': ObjectId(Oid)})
     if res.deleted_count:
@@ -95,12 +137,13 @@ def get_blogs_list(page: int, size: int):
     return dict(count=count, Page=page, data=res)
 
 
-@app.post('/blogs/')
-def filter_blogs(request: dict):
-    page = request['page']
-    size = request['size']
-    data = request['filters']
-    filters = [{k: data[k]} for k in data.keys()]
+@app.post('/blogs/', dependencies=[Depends(jwtBearer())])
+def filter_blogs(data: dict, request: Request):
+    print(request.headers)
+    page = data['page']
+    size = data['size']
+    filters_data = data['filters']
+    filters = [{k: filters_data[k]} for k in filters_data.keys()]
     count = len(list(DBClient[DBName][Collection].find({'$and': filters})))
     res = list(DBClient[DBName][Collection].find({'$and': filters}).skip(page * size).limit(size))
     for i in res:
@@ -108,14 +151,31 @@ def filter_blogs(request: dict):
     return dict(count=count, Page=page, data=res)
 
 
-@app.put('/blogs/')
-def search_blogs(request: dict):
-    page = request['page']
-    size = request['size']
-    data = request['filters']
-    filters = [{k: data[k]} for k in data.keys()]
+@app.put('/blogs/', dependencies=[Depends(jwtBearer())])
+def search_blogs(data: dict, request: Request):
+    print(request.headers)
+    page = data['page']
+    size = data['size']
+    filters_data = data['filters']
+    filters = [{k: filters_data[k]} for k in filters_data.keys()]
     count = len(list(DBClient[DBName][Collection].find({'$or': filters})))
     res = list(DBClient[DBName][Collection].find({'$or': filters}).skip(page * size).limit(size))
+    for i in res:
+        i['_id'] = str(i['_id'])
+    return dict(count=count, Page=page, data=res)
+
+
+@app.patch('/blogs/', dependencies=[Depends(jwtBearer())])
+def list_blogs_in_date_range(request: dict):
+    page = request['page']
+    size = request['size']
+    count = len(
+        list(DBClient[DBName][Collection].find(
+            {'published_at': {'$gt': datetime.strptime(request['startdate'], '%Y-%m-%d'),
+                              "$lt": datetime.strptime(request['enddate'], '%Y-%m-%d')}})))  # '2032-04-25'
+    res = list(DBClient[DBName][Collection].find({'published_at': {
+        '$gt': datetime.strptime(request['startdate'], '%Y-%m-%d'),
+        '$lt': datetime.strptime(request['enddate'], '%Y-%m-%d')}}).skip(page * size).limit(size))
     for i in res:
         i['_id'] = str(i['_id'])
     return dict(count=count, Page=page, data=res)
