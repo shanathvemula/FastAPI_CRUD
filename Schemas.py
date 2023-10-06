e983c262b8288738d4c61bb23d8d5cded89804c3
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2023-10-06 17:56:33 +0530

    Updated JWT Authentication

--- Schemas.py
+++ Schemas.py
@@ -1,7 +1,10 @@
-from pydantic import BaseModel, Field, SecretStr
+from pydantic import BaseModel, Field, SecretStr, validator, root_validator
 from typing import Optional
 from datetime import datetime
 from uuid import UUID, uuid4
+from database import DBClient, DBName, UserCollection, Collection
+# from
+import re
 
 class Blog(BaseModel):
     # id: UUID = Field(default_factory=uuid4)
@@ -31,10 +34,29 @@ class Blog(BaseModel):
 
 class UserCreate(BaseModel):
     username: str | None
-    phoneno: int | None
+    phoneno: str | None
     email: str | None
     password: str = Field(min_length=8, max_length=25)
     age: None | int = Field(gt=17, lt=101)
+
+
+    @root_validator()
+    def validating_username_phoneno_email(cls, field_values):
+        if field_values['username'] or field_values['phoneno'] or field_values['email']:
+            if not list(DBClient[DBName][UserCollection].find({'$or':[{'username':field_values['username']},{'phoneno':field_values['phoneno']},{'email':field_values['email']}]})):
+                return field_values
+            raise ValueError("Already user exists with his details")
+        else:
+            raise ValueError("Provide valid information for username, phoneno, email")
+
+    @validator('email')
+    def email_Valitor(cls, value):
+        regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+        if value:
+            if not re.fullmatch(regex, value):
+                raise ValueError("Please provide valid Email")
+        return value
+
 
 class User(UserCreate):
     id: UUID = Field(default_factory=uuid4)
