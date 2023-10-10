2fe2f134bb7d8715d2b5c0dfd06e52029d068203
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2023-10-10 13:27:10 +0530

    Resisting multiple host to 2

--- authentication.py
+++ authentication.py
@@ -4,15 +4,14 @@ import time
 import jwt
 from Schemas import User
 import datetime
-from database import DBClient, DBName
-
+from database import DBClient, DBName, UserCollection
 from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 secret = 'c427efa41a05507e7bd9637270ff5a20db6ad20f2dbb0cf8d5bbf5af4f89aea5'
 algorithm = 'HS256'
-AccessTimeDelta = 600  # Seconds
+AccessTimeDelta = 60  # Seconds
 RefreshTimeDelta = 600  # Seconds
 
 
@@ -20,19 +19,31 @@ RefreshTimeDelta = 600  # Seconds
 def AccessToken(cred: dict):
     try:
         user = list(
-            DBClient[DBName]['user'].find({'$and': [
+            DBClient[DBName][UserCollection].find({'$and': [
                 {'$or': [{'username': cred['username']}, {'phoneno': cred['username']}, {'email': cred['username']}]},
                 {'password': cred['password']}]}))
-        # print(user[0]['username'])
-        if user:
-            encode = jwt.encode({'username': user[0]['username'], 'iat': datetime.datetime.now(tz=datetime.timezone.utc),
-                                 'exp': datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(
-                                     seconds=AccessTimeDelta)},
-                                key=secret,
-                                algorithm=algorithm)
+        # print(user[0]['Additional'] == {} or 'Login' not in user[0]['Additional'].keys())
+        if user[0]['Additional'] == {} or 'Login' not in user[0]['Additional'].keys():
+            user[0]['Additional']['Login'] = [cred['host']]
+            DBClient[DBName][UserCollection].update_one({'_id': user[0]['_id']}, {'$set': user[0]})
+        # print(user[0]['Additional']['Login'])
+        # print((cred['host'] not in user[0]['Additional']['Login']) and (len(user[0]['Additional']['Login']) < 2))
+        if cred['host'] not in user[0]['Additional']['Login'] and len(user[0]['Additional']['Login']) < 2:
+            user[0]['Additional']['Login'].append(cred['host'])
+            DBClient[DBName][UserCollection].update_one({'_id': user[0]['_id']}, {'$set': user[0]})
+
+        if user and cred['host'] in user[0]['Additional']['Login']:
+            encode = jwt.encode(
+                {'username': user[0]['username'], 'iat': datetime.datetime.now(tz=datetime.timezone.utc),
+                 'exp': datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(
+                     seconds=AccessTimeDelta)},
+                key=secret,
+                algorithm=algorithm)
             # print(datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(minutes=1))
             # print(encode)
             return {'token': encode}
+        elif len(user[0]['Additional']['Login']) >= 2:
+            raise MemoryError("You are login device limit over")
         else:
             return {"Error": "With given credential user doesn't exists"}
 
@@ -74,10 +85,13 @@ def VerifyToken(data):
 
 def decodeJWT(token: str) -> dict:
     try:
+        token = token.replace(' ', '')
         decoded_token = jwt.decode(token, secret, algorithms=[algorithm])
-        return decoded_token if decoded_token["expires"] >= time.time() else None
-    except:
-        return {}
+        print(decoded_token["exp"])
+        print(int(datetime.datetime.now().timestamp()))
+        return decoded_token if decoded_token["exp"] >= int(datetime.datetime.now().timestamp()) else None
+    except Exception as e:
+        return str(e)
 
 
 # Verifying access token from the Headers
@@ -89,12 +103,14 @@ class jwtBearer(HTTPBearer):
     async def __call__(self, request: Request):
         credentials: HTTPAuthorizationCredentials = await super(jwtBearer, self).__call__(request)
         if credentials:
-            # print(credentials)
             if not credentials.scheme == 'Bearer':
                 raise HTTPException(status_code=403, detail="Invalid or Expered Token")
+            data = decodeJWT(credentials.credentials)
+            if data == "Signature has expired":
+                raise HTTPException(status_code=403, detail="Signature has expired")
             return credentials.credentials
         else:
-            return HTTPException(status_code=403, detail="Invalid or Expered Token")
+            raise HTTPException(status_code=403, detail="Invalid or Expired Token")
 
     def verify_jwt(self, jwtoken: str):
         isTokenValid: bool = False  # Default Flag
